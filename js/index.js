cb30c7704a5165b35165f6c9857cd5ea3f2b3114
dinhhung2211 <dinhhunglqd2211@gmail.com>
2021-03-09 20:06:49 +0700

    update source code layout product

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+   $('#rateit').on('click', function(){
+        console.log($('#rateit').value());
+   });
+});
+
+var a = function(){
+    console.log("click");
+}
